24d3ff4cded91a51c5ea87ac7ae596e0a9f7b86f
ChrisDMorrall <chris.morrall@hotmail.co.uk>
2018-05-01 23:07:41 +0200

    S14 L58&59 Set up asynchronous searches for posts andpages

--- app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/js/modules/Search.js
@@ -44,14 +44,20 @@ class Search {
     this.previousValue = this.searchField.val();
   }
   getResults() {
-    $.getJSON(universityData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
+    $.when(
+      $.getJSON(universityData.root_url + '/wp-jsonxxxx/wp/v2/posts?search=' + this.searchField.val()),
+      $.getJSON(universityData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val()))
+      .then((posts, pages) => {
+      var combinedResults = posts[0].concat(pages[0]);
       this.resultsDiv.html(`
         <h2 class="search-overlay__section-title">Search Results</h2>
-        ${posts.length ? '<ul class="link-list min-list">' : '<p>No results found</p>'}
-          ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-        ${posts.length ? '</ul>' : ''}
+        ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No results found</p>'}
+          ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+        ${combinedResults.length ? '</ul>' : ''}
       `);
       this.isSpinnerVisible = false;
+    },() => {
+      this.resultsDiv.html('<p>Unexpected error please try again.</p>')
     });
   }
 
