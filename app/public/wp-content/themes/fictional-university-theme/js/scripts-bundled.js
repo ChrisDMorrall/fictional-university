d03e5d215a4b93fb2f01b35442ffb18a09774ff7
ChrisDMorrall <chris.morrall@hotmail.co.uk>
2018-05-16 12:40:25 +0200

    S20 L90 - completed like code delete function and the course!

--- app/public/wp-content/themes/fictional-university-theme/js/scripts-bundled.js
+++ app/public/wp-content/themes/fictional-university-theme/js/scripts-bundled.js
@@ -13942,7 +13942,7 @@ function () {
     value: function ourClickDispatcher(e) {
       var currentLikeBox = (0, _jquery.default)(e.target).closest(".like-box");
 
-      if (currentLikeBox.data('exists') == 'yes') {
+      if (currentLikeBox.attr('data-exists') == 'yes') {
         this.deleteLike(currentLikeBox);
       } else {
         this.createLike(currentLikeBox);
@@ -13961,6 +13961,11 @@ function () {
           'professorId': currentLikeBox.data('professor')
         },
         success: function success(response) {
+          currentLikeBox.attr('data-exists', 'yes');
+          var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+          likeCount++;
+          currentLikeBox.find(".like-count").html(likeCount);
+          currentLikeBox.attr("data-like", response);
           console.log(response);
         },
         error: function error(response) {
@@ -13972,9 +13977,20 @@ function () {
     key: "deleteLike",
     value: function deleteLike(currentLikeBox) {
       _jquery.default.ajax({
+        beforeSend: function beforeSend(xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', universityData.nonce);
+        },
         url: universityData.root_url + '/wp-json/university/v1/manageLike',
+        data: {
+          'like': currentLikeBox.attr('data-like')
+        },
         type: 'DELETE',
         success: function success(response) {
+          currentLikeBox.attr('data-exists', 'no');
+          var likeCount = parseInt(currentLikeBox.find(".like-count").html(), 10);
+          likeCount--;
+          currentLikeBox.find(".like-count").html(likeCount);
+          currentLikeBox.attr("data-like", '');
           console.log(response);
         },
         error: function error(response) {
