49e1cc7f1b94daaf7fc619abbf1772def82f1373
ChrisDMorrall <chris.morrall@hotmail.co.uk>
2018-05-16 10:44:03 +0200

    S20 L88 - updated Like.js to make ajax call to create and delete likes

--- app/public/wp-content/themes/fictional-university-theme/inc/like-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/like-route.php
@@ -7,14 +7,24 @@ function universityLikeRoutes() {
     'methods' => 'POST',
     'callback' => 'createLike'
   ));
+
   register_rest_route('university/v1', 'manageLike', array(
     'methods' => 'DELETE',
     'callback' => 'deleteLike'
   ));
 }
 
-function createLike() {
-  return 'Thanks for trying to create a like';
+function createLike($data) {
+  $professor = sanitize_text_field($data['professorId']);
+
+  wp_insert_post(array(
+    'post_type' => 'like',
+    'post_status' => 'publish',
+    'post_title' => '2nd PHP Test',
+    'meta_input' => array(
+      'liked_professor_id' => $professor
+    )
+  ));
 }
 
 function deleteLike() {
