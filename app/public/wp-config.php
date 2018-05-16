e03e107b8c67ecfd4385940c0eab6dc6cc60f221
ChrisDMorrall <chris.morrall@hotmail.co.uk>
2018-05-16 13:54:23 +0200

    Changed wp-config.php and added local.php to take care of local and live database settings

--- app/public/wp-config.php
+++ app/public/wp-config.php
@@ -19,17 +19,21 @@
  */
 
 // ** MySQL settings ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
+if (file_exists(dirname(__FILE__) . '/local.php')) {
+    // Local database settings
+    define( 'DB_NAME', 'local' );
+    define( 'DB_USER', 'root' );
+    define( 'DB_PASSWORD', 'root' );
+    define( 'DB_HOST', 'localhost' );
+} else {
+    // Live database settings
+    define( 'DB_NAME', 'universitydata-33354aeb' );
+    define( 'DB_USER', 'universitydata-33354aeb' );
+    define( 'DB_PASSWORD', 'srbuabcf06' );
+    define( 'DB_HOST', 'shareddb-i.hosting.stackcp.net' );
+}
 
-/** MySQL database username */
-define( 'DB_USER', 'root' );
 
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
